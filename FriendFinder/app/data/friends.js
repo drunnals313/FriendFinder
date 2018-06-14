41b1c264cece9edd0864838fa68d18170cc575c5
drunnals313 <davidrunnals@gmail.com>
2018-06-13 20:42:09 -0400

    updated html and js files and connections

--- FriendFinder/app/data/friends.js
+++ FriendFinder/app/data/friends.js
@@ -1,24 +1,165 @@
-// ===============================================================================
-// DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
-// ===============================================================================
-
-var tableArray = [
+// Data structure that will store the friends data
+var friendsArray = [
 	{
-		customerName: "Ahmed",
-		customerEmail: "ahmed@example.com",
-		customerID: "afhaque89",
-		phoneNumber: "000-000-0000"
+		name: 'Sophia',
+		photo: '/images/sophia.jpg',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'3',
+			'2',
+			'1',
+			'5',
+			'5',
+			'3',
+			'1'
+		]
 	},
 	{
-		customerName: "George",
-		customerEmail: "george@costanza.com",
-		customerID: "geo89",
-		phoneNumber: "212-312-2431"
+		name: 'Olivia',
+		photo: '/images/olivia.jpg',
+		scores: [
+			'5',
+			'4',
+			'1',
+			'2',
+			'4',
+			'5',
+			'3',
+			'2',
+			'1',
+			'4'
+		]
+	},
+	{
+		name: 'Emma',
+		photo: '/images/emma.jpg',
+		scores: [
+			'2',
+			'1',
+			'1',
+			'4',
+			'1',
+			'2',
+			'4',
+			'3',
+			'2',
+			'1'
+		]
+	},
+	{
+		name: 'Ava',
+		photo: '/images/ava.jpg',
+		scores: [
+			'5',
+			'4',
+			'3',
+			'1',
+			'2',
+			'3',
+			'4',
+			'3',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Isabella',
+		photo: '/images/isabella.jpg',
+		scores: [
+			'2',
+			'3',
+			'4',
+			'4',
+			'5',
+			'5',
+			'3',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Jackson',
+		photo: '/images/jackson.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'4',
+			'3',
+			'4',
+			'5',
+			'5',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Liam',
+		photo: '/images/liam.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Noah',
+		photo: '/images/noah.jpg',
+		scores: [
+			'4',
+			'3',
+			'2',
+			'1',
+			'5',
+			'4',
+			'4',
+			'1',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Aiden',
+		photo: '/images/aiden.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'3',
+			'4',
+			'1',
+			'5',
+			'2',
+			'5',
+			'3'
+		]
+	},
+	{
+		name: 'Lucas',
+		photo: '/images/lucas.jpg',
+		scores: [
+			'2',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
 	}
 ];
 
-// Note how we export the array. This makes it accessible to other files using require.
-module.exports = tableArray;
+module.exports = friendsArray;
